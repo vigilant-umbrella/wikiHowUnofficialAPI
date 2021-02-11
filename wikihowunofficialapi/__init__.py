45bfbf28ef2839f865adf4ba00034bfd5dc9d80f
Ashok Arora <arora8715@gmail.com>
2021-02-11 20:50:22 +0400

    Code comments

--- wikihowunofficialapi/__init__.py
+++ wikihowunofficialapi/__init__.py
@@ -27,21 +27,46 @@ class Steps:
 
     @property
     def number(self):
+        """Method to return the count of the current method.
+
+        Returns:
+            int: The count of the current method
+        """
         return self._number
 
     @property
     def title(self):
+        """Method to return the title of a step.
+
+        Returns:
+            str: The title of a step
+        """
         return self._title
 
     @property
     def description(self):
+        """Method to return the description given in a step.
+
+        Returns:
+            str: The description given in a step
+        """
         return self._description
 
     @property
     def picture(self):
+        """Method to return the URL of image associated with the step.
+
+        Returns:
+            str: The URL of image associated with the step
+        """
         return self._picture
 
     def get(self):
+        """Method to return a dictionary of Steps data members.
+
+        Returns:
+            dict: A dictionary of Steps data members
+        """
         return {
             'number': self.number,
             'title': self.title,
@@ -61,17 +86,37 @@ class Methods:
 
     @property
     def number(self):
+        """Method to return the count of the current method.
+
+        Returns:
+            int: The count of the current method
+        """
         return self._number
 
     @property
     def title(self):
+        """Method to return the title of a method.
+
+        Returns:
+            str: The title of a method
+        """
         return self._title
 
     @property
     def steps(self):
+        """Method to return a list of steps in a method.
+
+        Returns:
+            list: A list of steps in a method
+        """
         return self._steps
 
     def get(self):
+        """Method to return a dictionary of Method data members.
+
+        Returns:
+            dict: A dictionary of Method data members
+        """
         return {
             'number': self.number,
             'title': self.title,
@@ -105,93 +150,175 @@ class Article:
 
     @property
     def url(self):
+        """Method to return the URL of a wikiHow article.
+
+        Returns:
+            str: The wikiHow article URL
+        """
         if not self._parsed:
             self._parse()
         return self._url
 
     @property
     def title(self):
+        """Method to return the title of a wikiHow article.
+
+        Returns:
+            str: The wikiHow article title
+        """
         if not self._parsed:
             self._parse()
         return self._title
 
     @property
     def intro(self):
+        """Method to return the introduction from a wikiHow article.
+
+        Returns:
+            str: The wikiHow article introduction
+        """
         if not self._parsed:
             self._parse()
         return self._intro
 
     @property
     def methods(self):
+        """Method to return the method titles from a wikiHow article.
+
+        Returns:
+            list: A list of method titles
+        """
         if not self._parsed:
             self._parse()
         return self._methods
 
     @property
     def n_methods(self):
+        """Method to return the number of methods in a wikiHow article.
+
+        Returns:
+            int: The number of methods in a wikiHow article
+        """
         return len(self._methods)
 
     @property
     def num_votes(self):
+        """Method to return the number of votes given to a wikiHow article.
+
+        Returns:
+            int: The number of votes given to a wikiHow article
+        """
         if not self._parsed:
             self._parse()
         return self._num_votes
 
     @property
     def percent_helpful(self):
+        """Method to return the percent of helpful recieved by a wikiHow article.
+
+        Returns:
+            int: The percent of helpful recieved by a wikiHow article
+        """
         if not self._parsed:
             self._parse()
         return self._percent_helpful
 
     @property
     def is_expert(self):
+        """Method to check if a wikiHow article is written by an expert.
+
+        Returns:
+            bool: True if written by an expert, False otherwise.
+        """
         if not self._parsed:
             self._parse()
         return self._is_expert
 
     @property
     def last_updated(self):
+        """Method to return the last date when a wikiHow article was updated. (Format: YYYY-MM-DD HH: MM: SS)
+
+        Returns:
+            str: The last date when a wikiHow article was updated.
+        """
         if not self._parsed:
             self._parse()
         return self._last_updated
 
     @property
     def views(self):
+        """Method to return the number of times a wikiHow article was viewed.
+
+        Returns:
+            int: Number of times a wikiHow article was viewed.
+        """
         if not self._parsed:
             self._parse()
         return self._views
 
     @property
     def co_authors(self):
+        """Method to return the number of co-authors in a wikiHow article.
+
+        Returns:
+            int: Number of co-authors in a wikiHow article.
+        """
         if not self._parsed:
             self._parse()
         return self._co_authors
 
     @property
     def references(self):
+        """Method to return the number of references in a wikiHow article.
+
+        Returns:
+            int: Number of references in a wikiHow article.
+        """
         if not self._parsed:
             self._parse()
         return self._references
 
     @property
     def summary(self):
+        """Method to return the summary of a wikiHow article.
+
+        Returns:
+            str: summary of a wikiHow article.
+        """
         if not self._parsed:
             self._parse()
         return self._summary
 
     @property
     def warnings(self):
+        """Method to return a list of warnings from a wikiHow article.
+
+        Returns:
+            list: Warnings from a wikiHow article.
+        """
         if not self._parsed:
             self._parse()
         return self._warnings
 
     @property
     def tips(self):
+        """Method to return a list of tips from a wikiHow article.
+
+        Returns:
+            list: Tips from a wikiHow article.
+        """
         if not self._parsed:
             self._parse()
         return self._tips
 
     def _parse_title(self, soup):
+        """Method to extract the title of a wikiHow article.
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Raises:
+            ParseError: The given article could not be parsed.
+        """
         html = soup.findAll(
             'h1', {'class': ['title_lg', 'title_md', 'title_sm']})[0]
         if not html.find('a'):
@@ -203,6 +330,13 @@ class Article:
             self._title = self._url.split('/')[-1].replace('-', ' ')
 
     def _parse_intro(self, soup):
+        """Method to extract the introduction from a wikiHow article.
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Raises:
+            ParseError: The given article could not be parsed.
+        """
         intro_html = soup.find('div', {'class': 'mf-section-0'})
         if not intro_html:
             raise ParseError
@@ -218,6 +352,13 @@ class Article:
                 self._intro = intro.strip()
 
     def _parse_methods(self, soup):
+        """Method to extract the methods from a wikiHow article.
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Raises:
+            ParseError: The given article could not be parsed.
+        """
         self._methods = []
         methods_html = soup.findAll(
             'div', {'class': ['section steps steps_first sticky', 'section steps sticky']})
@@ -276,6 +417,13 @@ class Article:
                     self._methods[count-1]._steps.append(step)
 
     def _parse_votes_n_helpful(self, soup):
+        """Method to extract the number of helpful votes and helpful percentage given to a wikiHow article.
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Raises:
+            ParseError: The given article could not be parsed.
+        """
         num_votes_html = soup.find('div', {'class': 'sp_helpful_rating_count'})
         if num_votes_html:
             if str(num_votes_html) == '<div class="sp_helpful_rating_count"></div>':
@@ -287,6 +435,14 @@ class Article:
                 '- ')+2:content.find('%</div>')])
 
     def _parse_is_expert(self, soup):
+        """Method to check if a wikiHow article is written by an expert.
+
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Raises:
+            ParseError: The given article could not be parsed.
+        """
         expert_html = soup.find('div', {'id': 'byline_info'})
         if not expert_html:
             raise ParseError
@@ -298,6 +454,14 @@ class Article:
                 self._is_expert = False
 
     def _parse_last_updated(self, soup):
+        """Method to extract the date of last update of a wikiHow article.
+
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Raises:
+            ParseError: The given article could not be parsed.
+        """
         update_html = soup.find('div', {'id': 'byline_info'})
         if not update_html:
             raise ParseError
@@ -307,6 +471,14 @@ class Article:
             self._last_updated = datetime.strptime(date, '%B %d, %Y')
 
     def _parse_views(self, soup):
+        """Method to extract the number of views in a wikiHow article.
+
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Raises:
+            ParseError: The given article could not be parsed.
+        """
         views_html = soup.find('div', {'class': 'sp_box sp_stats_box'})
         if not views_html:
             raise ParseError
@@ -317,9 +489,17 @@ class Article:
                 (span[span.find('>')+1: span.find('</span>')]).split(',')))
 
     def _parse_co_authors(self, soup):
+        """Method to extract the number of co-authors in a wikiHow article.
+
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Returns:
+            None: When no co-authors are found.
+        """
         co_authors_html = soup.find('div', {'class': 'sp_box sp_stats_box'})
         if not co_authors_html:
-            raise ParseError
+            return None
         else:
             div = co_authors_html.findAll('div', {'class': 'sp_text'})
             span = str(div[0].find('span', {'class': 'sp_text_data'}))
@@ -327,6 +507,14 @@ class Article:
                 (span[span.find('>')+1: span.find('</span>')]).split(',')))
 
     def _parse_references(self, soup):
+        """Method to extract the number of references in a wikiHow article.
+
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Returns:
+            None: When no references are found.
+        """
         references_html = soup.findAll('a', {'class': 'external free'})
         count = 0
         if not references_html:
@@ -337,6 +525,14 @@ class Article:
         self._references = count
 
     def _parse_summary(self, soup):
+        """Method to extract summary from a wikiHow article.
+
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Returns:
+            None: When no summary is found.
+        """
         summary_html_div = soup.find('div', {'id': 'summary_wrapper'})
         if not summary_html_div:
             return None
@@ -346,6 +542,14 @@ class Article:
             self._summary = summary
 
     def _parse_warnings(self, soup):
+        """Method to extract warnings from a wikiHow article.
+
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Returns:
+            None: When no warnings are found.
+        """
         warnings_html_div = soup.find('div', {'id': 'warnings'})
         if not warnings_html_div:
             return None
@@ -364,6 +568,14 @@ class Article:
                     return None
 
     def _parse_tips(self, soup):
+        """Method to extract tips from a wikiHow article.
+
+        Args:
+            soup(bs4.BeautifulSoup): An instance of BeautifulSoup class for the entire article.
+
+        Returns:
+            None: When no tips are found.
+        """
         tips_html_div = soup.find('div', {'id': 'tips'})
         if not tips_html_div:
             return None
@@ -383,6 +595,11 @@ class Article:
                     return None
 
     def _parse(self):
+        """Method to extract useful information from a given wikiHow article.
+
+        Raises:
+            ParseError: The given article could not be parsed.
+        """
         try:
             content = urllib.request.urlopen(self._url)
             read_content = content.read()
@@ -405,6 +622,11 @@ class Article:
             raise ParseError
 
     def get(self):
+        """Method to return a dictionary of class members.
+
+        Returns:
+            dict: A dictionary of class members.
+        """
         return {
             'url': self.url,
             'title': self.title,
@@ -448,6 +670,19 @@ class WikiHow:
 
     @ staticmethod
     def search(search_term, max_results=-1, lang='en'):
+        """Method to search for wikiHow articles.
+
+        Args:
+            search_term(str): [description]
+            max_results(int, optional): Number of results. Defaults to - 1.
+            lang(str, optional):  Language of the wikiHow articles. Defaults to 'en'.
+
+        Raises:
+            UnsupportedLanguage: There are no wikiHow articles with this language.
+
+        Yields:
+            str: One of the search results.
+        """
         lang = lang.split('-')[0].lower()
         if lang not in WikiHow.lang2url:
             raise UnsupportedLanguage
@@ -474,13 +709,32 @@ class WikiHow:
 
 
 def random_article(lang='en'):
+    """Method to return a random wikiHow article.
+
+    Args:
+        lang(str, optional): Language of the wikiHow article. Defaults to 'en'.
+
+    Returns:
+        str: URL of the article.
+    """
     url = WikiHow.lang2url[lang] + 'Special:Randomizer'
     return Article(url)
 
 
 def search_wikihow(query, max_results=10, lang='en'):
+    """Method to search and return a list of wikHow articles.
+
+    Args:
+        query(str): Search string
+        max_results(int, optional): Number of search results. Defaults to 10.
+        lang(str, optional): Language of the wikiHow articles. Defaults to 'en'.
+
+    Returns:
+        list: A list containing the names of the Wikhow articles from the search result.
+    """
     return list(WikiHow.search(query, max_results, lang))
 
 
 if __name__ == '__main__':
+    """This file can only be loaded as a module."""
     exit()
