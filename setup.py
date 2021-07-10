17e57e75165447a4f76ada28769fec84fd34fbf9
Aniket Sharma <aniketsharma00411@gmail.com>
2021-07-10 15:26:28 +0530

    New release (#15)

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name='wikihowunofficialapi',
-    version='0.1.0',
+    version='0.1.1',
     description='API to extract data from wikiHow',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/vigilant-umbrella/wikihowunofficialapi',
-    download_url='https://github.com/vigilant-umbrella/wikihowunofficialapi/archive/v0.1.0.tar.gz',
+    download_url='https://github.com/vigilant-umbrella/wikihowunofficialapi/archive/v0.1.1.tar.gz',
     author='Aniket Sharma, Ashok Arora',
     author_email='vigilant.umbrella@gmail.com',
     license='MIT',
